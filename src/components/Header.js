98ec544ce28646aee6f470e548b834a4a3b39497
MaxiColombo~ <maximocolombo2003@gmail.com>
2022-08-03 21:14:22 -0300

    Creamos el git de React+

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import logo from "../assets/image/logo_duende.png"
+
+function Header() {
+  return (
+    <div><img src={logo} alt="Logo"></img></div>
+  )
+}
+
+export default Header
---
+++ src/components/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './Header';
+
+function Home() {
+  return (
+<React.Fragment>  
+    <div className="Home">
+     <header>
+      <Header/>
+     </header>
+    </div>
+    </React.Fragment>
+  );
+}
+
+export default Home;
