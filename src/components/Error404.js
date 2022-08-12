f06a3545a13a551320ecd55295402ad88f2680fb
Lucas8353 <lucas531@hotmail.com>
2022-08-12 18:31:57 -0300

    error 404 con css

--- src/components/Error404.js
+++ src/components/Error404.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import error from "../assets/image/error.gif"
+import background from "../assets/image/background.jpg"
 
 const Error404 = () => {
   return (
-    <div>Error404</div>
+    <div class="not-found">
+          <div>
+{/*       <img class= "backgrund" src={background} /> */}
+        <p class="image" />
+    <img class="gif" src={error} alt="gif"/>
+
+    <h1>Ups... Parece que esta pagina no existe!!</h1>
+    <p >Te invitamos a seguir explorando nuestros productos</p>
+    <p class="button"/>
+    <a href="/"><button>Si pinta, Pinta</button></a>
+
+    </div>
+</div>
+
+
+
+
+
   )
 }
 
